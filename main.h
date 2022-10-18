bd170bcbfba4488544671d3a274af3a02747e067
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-10-18 09:08:44 -0700

    update

--- main.h
+++ main.h
@@ -1,24 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define NULL ((void *)0)
 
-#include <stdlib.h>
-#include <stdarg.h>
+
 
 int _putchar(char c);
-int _puts(char *str);
+int _print_str(char *string);
+int _print_int(long int var);
 int _printf(const char *format, ...);
-
-/**
-  *struct  Special_chr - all special charectors
-  *@chr: special charector
-  *@f: function
-  */
-
-typedef struct special_chr
-{
-	char *chr;
-	int (*f)(va_list);
-special_chr;
-}
+int to_Binary(unsigned int n);
+int to_Octal(unsigned int n);
+int _power_recursion(int x, int y);
+int to_Hexa(unsigned int num);
 
 
