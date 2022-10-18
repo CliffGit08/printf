fb0f20f03870b98035a481c3a5583ef0b871469b
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-10-18 08:59:25 -0700

    update

--- main.h
+++ main.h
@@ -1,42 +1,24 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
 #include <stdlib.h>
-
 #include <stdarg.h>
 
-
-
 int _putchar(char c);
-
 int _puts(char *str);
-
 int _printf(const char *format, ...);
 
-
-
 /**
-
   *struct  Special_chr - all special charectors
-
   *@chr: special charector
-
   *@f: function
-
   */
 
 typedef struct special_chr
-
 {
-
 	char *chr;
-
 	int (*f)(va_list);
-
-}special_chr;
-
+special_chr;
+}
 
 
